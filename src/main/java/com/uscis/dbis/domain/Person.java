82a64454f1fc71ed7bc4ab04457cc7346cd86f3d
echasin <echasin@innvosolutions.com>
2024-09-17 10:33:42 -0700

    Create Person.java domain objec

---
+++ src/main/java/com/uscis/dbis/domain/Person.java
@@ -0,0 +1,23 @@
+package com.uscis.dbis.domain;
+
+import org.springframework.data.neo4j.core.schema.GeneratedValue;
+import org.springframework.data.neo4j.core.schema.Id;
+import org.springframework.data.neo4j.core.schema.Node;
+
+@Node
+public class Person {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
